e00414f275b6d6c88835644ecbaedf7731a46b10
Nikkon <nikkonlawrance@gmail.com>
2022-03-16 11:45:51 +0600

    new component

--- realEstateApp/src/app/app.module.ts
+++ realEstateApp/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PropertyCradComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCradComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule,
